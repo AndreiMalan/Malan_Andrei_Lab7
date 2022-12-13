8585ca96d3a94bab6a30ecb5e5547657a89819d5
AndreiMalan <andreimalan10@gmail.com>
2022-12-13 16:21:27 +0200

    lab10

--- Models/ShopList.cs
+++ Models/ShopList.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 using SQLite;
+using SQLiteNetExtensions.Attributes;
 
 namespace Malan_Andrei_Lab7.Models
 {
@@ -17,5 +18,7 @@ namespace Malan_Andrei_Lab7.Models
         [MaxLength(250), Unique]
         public string Description { get; set; }
         public DateTime Date { get; set; }
+        [ForeignKey(typeof(Shop))]
+        public int ShopID { get; set; }
     }
 }
